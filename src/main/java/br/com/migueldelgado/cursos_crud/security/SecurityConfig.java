543437306839ff5abdd8a28d13537433ab039a2e
4Miguel2 <delgdevoleper@gmail.com>
2024-05-25 18:52:10 -0300

    recuperando jwt

--- src/main/java/br/com/migueldelgado/cursos_crud/security/SecurityConfig.java
+++ src/main/java/br/com/migueldelgado/cursos_crud/security/SecurityConfig.java
@@ -1,14 +1,20 @@
 package br.com.migueldelgado.cursos_crud.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+
+    @Autowired
+    private SecurityUserFilter securityUserFilter;
+
 
     /**
      * Define a cadeia de filtros de segurança para a aplicação.
@@ -23,15 +29,14 @@ public class SecurityConfig {
                     auth.requestMatchers("/admin/").permitAll();
                     auth.requestMatchers("/user/auth/").permitAll();
                     auth.anyRequest().authenticated();
-                        });
+                        })
+                .addFilterBefore(securityUserFilter, BasicAuthenticationFilter.class);
 
         return http.build();
     }
 
     @Bean
     public PasswordEncoder passwordEncoder(){
-
-
 
         return new BCryptPasswordEncoder();
     }
